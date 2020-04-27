2497471b716bc360d0807461e9e1cdfa9f1dd0df
Mariana <moraismariana83@gmail.com>
2020-04-27 19:02:05 -0300

    Updating landing

--- src/js/script.js
+++ src/js/script.js
@@ -1,57 +1,59 @@
-const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-
-  //Toggle Nav
-  burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-    burger.classList.toggle('toggle');
-  });
-}
-
-navSlide();
 
 
+// const navSlide = () => {
+//   const burger = document.querySelector('.burger');
+//   const nav = document.querySelector('.nav-links');
+
+//   //Toggle Nav
+//   burger.addEventListener('click', () => {
+//     nav.classList.toggle('nav-active');
+//     burger.classList.toggle('toggle');
+//   });
+// }
+
+// navSlide();
 
 
 
-const command = ['n', 'p', 'm', ' ', 'i', 'n', 's', 't', 'a', 'l', 'l', ' ', 'l', 'i', 'g', 'a', '-', 'p', 'r', 'o', 'g', 'r', 'a', 'm', 'a', 'd', 'o', 'r', 'e', 's'];
-const adder = 2;
-let message ='';
-let i = 0;
 
-const typeMessage = () => {
-  if(i < command.length) {
-     message += command[i];
-    container.textContent = message;
-    i++;
-  } else {
-    clearInterval(fullMessage);
-    setTimeout(()=> {
+
+// const command = ['n', 'p', 'm', ' ', 'i', 'n', 's', 't', 'a', 'l', 'l', ' ', 'l', 'i', 'g', 'a', '-', 'p', 'r', 'o', 'g', 'r', 'a', 'm', 'a', 'd', 'o', 'r', 'e', 's'];
+// const adder = 2;
+// let message ='';
+// let i = 0;
+
+// const typeMessage = () => {
+//   if(i < command.length) {
+//      message += command[i];
+//     container.textContent = message;
+//     i++;
+//   } else {
+//     clearInterval(fullMessage);
+//     setTimeout(()=> {
      
-      loadingContainer.style.opacity = 1;
-    }, 1000);
+//       loadingContainer.style.opacity = 1;
+//     }, 1000);
        
-  }
-}
+//   }
+// }
 
-const container = document.querySelector('.command');
-const blink = document.querySelector('.blink');
+// const container = document.querySelector('.command');
+// const blink = document.querySelector('.blink');
 
-let fullMessage = setInterval(typeMessage, 30);
+// let fullMessage = setInterval(typeMessage, 30);
 
-/*Feature */
-const lista = document.getElementById("feature-list");
-          const listaBotoes = Array.from(lista.children);
+// /*Feature */
+// const lista = document.getElementById("feature-list");
+//           const listaBotoes = Array.from(lista.children);
           
-          const textContainer = document.getElementById("feature-container");
-          const listaText = Array.from(textContainer.children);
+//           const textContainer = document.getElementById("feature-container");
+//           const listaText = Array.from(textContainer.children);
 
-          listaBotoes.forEach((botao, indexBotao) => {
-            botao.addEventListener("click", (e) => {
-              listaText.forEach((text, indexText) => {
-                const displayType = indexBotao === indexText ? "grid" : "none";
-                text.style.display = displayType;
-              });
-            });
-          });
+//           listaBotoes.forEach((botao, indexBotao) => {
+//             botao.addEventListener("click", (e) => {
+//               listaText.forEach((text, indexText) => {
+//                 const displayType = indexBotao === indexText ? "grid" : "none";
+//                 text.style.display = displayType;
+//               });
+//             });
+//           });
