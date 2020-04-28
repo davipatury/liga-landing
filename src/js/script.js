1d02b931e8031b003637b713ce999a73c97f7ee0
Mariana <moraismariana83@gmail.com>
2020-04-28 19:13:55 -0300

    Adding responsive.css

--- src/js/script.js
+++ src/js/script.js
@@ -1,59 +1,69 @@
+/*Terminal*/
+consoleText(['Hello World', '(・_・)ノ'], 'text1', ['yellow', 'white']);
+consoleText(['npm install liga'], 'text2', ['lightblue']);
+consoleText(['./sermembro', '⚡️'], 'text3', ['lightgreen']);
 
+function consoleText(words, id, colors) {
+ if (colors === undefined) colors = ['#fff'];
+ var visible = true;
+ var con = document.getElementById('console');
+ var letterCount = 1;
+ var x = 1;
+ var waiting = false;
+ var target = document.getElementById(id)
+ target.setAttribute('style', 'color:' + colors[0])
+ window.setInterval(function () {
 
-// const navSlide = () => {
-//   const burger = document.querySelector('.burger');
-//   const nav = document.querySelector('.nav-links');
+  if (letterCount === 0 && waiting === false) {
+   waiting = true;
+   target.innerHTML = words[0].substring(0, letterCount)
+   window.setTimeout(function () {
+    var usedColor = colors.shift();
+    colors.push(usedColor);
+    var usedWord = words.shift();
+    words.push(usedWord);
+    x = 1;
+    target.setAttribute('style', 'color:' + colors[0])
+    letterCount += x;
+    waiting = false;
+   }, 1000)
+  } else if (letterCount === words[0].length + 1 && waiting === false) {
+   waiting = true;
+   window.setTimeout(function () {
+    x = -1;
+    letterCount += x;
+    waiting = false;
+   }, 1000)
+  } else if (waiting === false) {
+   target.innerHTML = words[0].substring(0, letterCount)
+   letterCount += x;
+  }
+ }, 120)
+ window.setInterval(function () {
+  if (visible === true) {
+   con.className = 'console-underscore hidden'
+   visible = false;
 
-//   //Toggle Nav
-//   burger.addEventListener('click', () => {
-//     nav.classList.toggle('nav-active');
-//     burger.classList.toggle('toggle');
-//   });
-// }
+  } else {
+   con.className = 'console-underscore'
 
-// navSlide();
+   visible = true;
+  }
+ }, 400)
+}
 
+/*Feature*/
+const lista = document.getElementById("feature-list");
+const listaBotoes = Array.from(lista.children);
 
+const textContainer = document.getElementById("feature-container");
+const listaText = Array.from(textContainer.children);
 
-
-
-// const command = ['n', 'p', 'm', ' ', 'i', 'n', 's', 't', 'a', 'l', 'l', ' ', 'l', 'i', 'g', 'a', '-', 'p', 'r', 'o', 'g', 'r', 'a', 'm', 'a', 'd', 'o', 'r', 'e', 's'];
-// const adder = 2;
-// let message ='';
-// let i = 0;
-
-// const typeMessage = () => {
-//   if(i < command.length) {
-//      message += command[i];
-//     container.textContent = message;
-//     i++;
-//   } else {
-//     clearInterval(fullMessage);
-//     setTimeout(()=> {
-     
-//       loadingContainer.style.opacity = 1;
-//     }, 1000);
-       
-//   }
-// }
-
-// const container = document.querySelector('.command');
-// const blink = document.querySelector('.blink');
-
-// let fullMessage = setInterval(typeMessage, 30);
-
-// /*Feature */
-// const lista = document.getElementById("feature-list");
-//           const listaBotoes = Array.from(lista.children);
-          
-//           const textContainer = document.getElementById("feature-container");
-//           const listaText = Array.from(textContainer.children);
-
-//           listaBotoes.forEach((botao, indexBotao) => {
-//             botao.addEventListener("click", (e) => {
-//               listaText.forEach((text, indexText) => {
-//                 const displayType = indexBotao === indexText ? "grid" : "none";
-//                 text.style.display = displayType;
-//               });
-//             });
-//           });
+listaBotoes.forEach((botao, indexBotao) => {
+ botao.addEventListener("click", (e) => {
+  listaText.forEach((text, indexText) => {
+   const displayType = indexBotao === indexText ? "grid" : "none";
+   text.style.display = displayType;
+  });
+ });
+});
